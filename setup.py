6d8d45916b8ceb489dcddd655db35e1b3be5e637
Alex <alex.e.hatfield@gmail.com>
2019-03-23 02:52:52 -0400

    Missed history clean out.

--- setup.py
+++ setup.py
@@ -11,9 +11,6 @@ root = os.path.dirname(__file__)
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = ['Click>=6.0', ]
 setup_requirements = ['pytest-runner', ]
 test_requirements = ['pytest', ]
@@ -42,7 +39,7 @@ setup(
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='app-net',
     name='app-net',
